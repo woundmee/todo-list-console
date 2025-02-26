a67078ef61f5998afda7f155c2a8ce09573d7276
woundmee <woundmee@gmail.com>
2025-02-26 17:40:24 +0700

    update program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 using Interfaces;
 using Services;
+using Microsoft.VisualBasic;
 
 Console.Clear();
 
@@ -35,10 +36,18 @@ while (true)
 
     switch (userSetMenu)
     {
+        case 0:
+            Console.Clear();
+            myConsole.Menu();
+            break;
         case 1:
+            Console.Clear();
             tasks.Create();
             break;
         case 2:
+            Console.Clear();
+            tasks.ShowAll();
+            Console.WriteLine();
             int removeTaskNumber = 0;
             if (tasks.GetCountTasks() != 0)
             {
@@ -48,9 +57,11 @@ while (true)
             tasks.Remove(removeTaskNumber);
             break;
         case 3:
+            Console.Clear();
             tasks.ShowAll();
             break;
         case 4:
+            Console.Clear();
             tasks.Change(1);
             Console.WriteLine("Недоступно...");
             break;
--- services/TaskService.cs
+++ services/TaskService.cs
@@ -100,8 +100,6 @@ class TaskService : ITaskService
             }
 
         }
-
-
     }
 
 
